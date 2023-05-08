78eec80b945903b457839d7a70acfdf2237729b2
Firdavs375 <firdavss.3755@gmail.com>
2023-05-08 20:19:27 +0500

    flex

---
+++ index.js
@@ -0,0 +1,57 @@
+// 1. Write a function that takes an array of numbers as input and returns the sum of all the even numbers in the array.
+// sumOfEvens([1, 2, 3, 4, 5, 6]) => 12
+// 2.Write a function that takes an array of numbers as input and returns the largest number in the array.
+// flargestNumber( [10, 5, 20, 8, 15]) =>  20
+// 3. Write a function that takes two strings as input and returns true if the second string is a substring of the first string, and false otherwise.
+// let string1 = "hello world";
+// let string2 = "wor";
+// isSubstring(string1, string2) => true
+// 4. Create an object that contains information about a person, including their name, age, and email address. Write a function that takes this object as input and returns the person"s name and email address formatted as "Name: email@example.com".
+// formatPerson(person) // Output: John Doe: john.doe@example.com
+// 5. Write a function that takes an array of strings as input and returns a new array containing only the strings that have at least one uppercase letter.
+// let strings = ["hello", "WORLD", "foo", "bar", "BAZ"];
+// console.log(uppercaseStrings(strings)); // Output: ["WORLD", "BAZ"]
+// https://ijazulrehman.github.io/Minimalistic-Startup-Landing-Page/video-demo
+
+// let a = [1,2,3,4,5,6,7,8,9,10]
+
+// let 
+
+
+// 1
+
+// sumOfEvens([1, 2, 3, 4, 5, 6])
+
+// for(let i =0; i <= sumOfEvens; i++){
+//     if (i % 2 ===0) {
+//         console.log(i+i);
+//     }else{
+//         console.log(false);
+//     }
+// }
+
+
+// 2
+
+// flargestNumber[10, 5, 20, 8, 15]
+
+// console.log(flargestNumber.max[]);
+
+
+// 3
+
+// let string1 = "hello world";
+// let string2 = "hello world";
+
+// if (string1.length === string2.length) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 4
+
+// let name = prompt ("Ism kiriting") 
+// let name2 = prompt ("Familya kiriting")
+
+// console.log(`${name}.${name2}@example.com`);
